8b2ac6a28285023b530ac737fbe1b28eae6d9729
DeveloperUchida <kouei28poiou@icloud.com>
2024-03-22 20:00:00 +0900

    DataUpdate 2024/03/22

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 import(
-	
+	"fmt"
+	"net/http"
+	"github.com/gin-going/gin"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+//Post データベース内の投稿を示す構造体
+type Post struct{
+	ID        bson.ObjectId `bson:"_id,omitempty"`
+    Content   string        `bson:"content"`
+    Author    string        `bson:"author"`
+    Timestamp int64         `bson:"timestamp"`
+}
+var (
+	session    *mgo.Session
+    dbName     = "somedb"
+    collection = "posts"
+)
+func main(){
+	
+}
